8ade03093d0cc6d00045f8c4a2d1bb176fc016c8
Leo Jansen <ljansenota@gmail.com>
2020-11-18 12:57:39 +0100

    Enemy movement

---
+++ Project Innovation/Assets/Scripts/Enemy Script/MoveTrail.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrail : MonoBehaviour
+{
+    //=======================================================================================
+    //                            >  Components & Variables  <
+    //=======================================================================================
+
+    // public objects
+    [Header("Components")]
+    public GameObject[] wayPoints;
+    
+
+
+    [Header("Variables")]
+    public float speed;
+
+    // private variables
+    private int _currentWaypoint;
+
+    //=======================================================================================
+    //                              >  Start And Update  <
+    //=======================================================================================
+    private void Start()
+    {
+        goToNextWavePoint();
+    }
+    private void Update()
+    {
+        moveForward();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Detected: other tag is: " + other.gameObject.tag);
+        if (other.gameObject.tag == "WaypointMarker")
+        {
+            goToNextWavePoint();
+        }
+    }
+    //=======================================================================================
+    //                              >  Start Functions  <
+    //=======================================================================================
+
+    //-----------------------------------privateFunctionName---------------------------------
+    //Description of function 
+    private void verbNoun(int pVarName) { }
+
+    //-----------------------------------PublicFunctionName-----------------------------------------
+    //Description of function 
+    private void VerbNoun(int pVarName) { }
+
+    //=======================================================================================
+    //                              >  Update Functions <
+    //=======================================================================================
+    //------------------------------------moveForward----------------------------------------
+    //Move enemy forward with current rotation
+    private void moveForward() 
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    //----------------------------------goToNextWaypoint-------------------------------------
+    //Rotate towards next waypoint
+    private void goToNextWavePoint()
+    {
+        //Get next waypoint
+        if (_currentWaypoint + 1 < wayPoints.Length)
+        {
+            _currentWaypoint++;
+        }
+        else
+        {
+            _currentWaypoint = 0;
+        }
+
+        //Change rotation to that waypoint
+        transform.LookAt(wayPoints[_currentWaypoint].transform);
+    }
+
+    //=======================================================================================
+    //                              >  Tool Functions  <
+    //=======================================================================================
+}
