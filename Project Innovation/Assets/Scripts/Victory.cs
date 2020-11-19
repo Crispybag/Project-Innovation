549eb0d4384c7b7db5d7a3e5b870a8bf65a4f94d
lekiam25 <42803736+lekiam25@users.noreply.github.com>
2020-11-19 13:01:59 +0100

    Added Victory Script

---
+++ Project Innovation/Assets/Scripts/Victory.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Victory : MonoBehaviour
+{
+    //=======================================================================================
+    //                            >  Components & Variables  <
+    //=======================================================================================
+
+    // public objects
+    [Header("Components")]
+    public Canvas victoryCanvasPrefab;
+
+    // public variables
+    //[Header("Variables")]
+
+    // private objects
+
+    // private variables
+
+    //=======================================================================================
+    //                              >  Start And Update  <
+    //=======================================================================================
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        WinIfKeyDown(KeyCode.Space);
+    }
+
+    //=======================================================================================
+    //                              >  Start Functions  <
+    //=======================================================================================
+
+    //-----------------------------------privateFunctionName-----------------------------------------
+    //Description of function 
+    private void verbNoun(int pVarName) { }
+
+    //-----------------------------------WinIfKeyDown-----------------------------------------
+    //Changes to the victory screen when you press the specified key 
+    public void WinIfKeyDown(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            Instantiate(victoryCanvasPrefab);
+        }
+    }
+
+    //=======================================================================================
+    //                              >  Update Functions <
+    //=======================================================================================
+
+    //=======================================================================================
+    //                              >  Tool Functions  <
+    //=======================================================================================
+}
