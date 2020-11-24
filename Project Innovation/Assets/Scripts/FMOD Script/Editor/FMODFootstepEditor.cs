b5498ce1a623ee92a0c511920b07a785d85a20ec
Premadness <65652200+Premadness@users.noreply.github.com>
2020-11-24 15:47:25 +0100

    Fmod build fix and fmod settings

---
+++ Project Innovation/Assets/Scripts/FMOD Script/Editor/FMODFootstepEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(FMODStudioMaterialSetter))]
+public class FMODFootstepEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var MS = target as FMODStudioMaterialSetter;
+        var FPF = FindObjectOfType<AdaptiveFootSteps>();
+
+        MS.materialValue = EditorGUILayout.Popup("Set Material As", MS.materialValue, FPF.materialTypes);
+    }
+}
+
+
+[CustomEditor(typeof(AdaptiveFootSteps))]
+public class FMODFootstepEditorTwo : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var FPF = target as AdaptiveFootSteps;
+
+        FPF.defaultMaterial = EditorGUILayout.Popup("Set Default Material As", FPF.defaultMaterial, FPF.materialTypes);
+    }
+}
+
