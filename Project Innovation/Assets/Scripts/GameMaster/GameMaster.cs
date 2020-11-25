6c4868892f591123deae90f74af8db50bc205dbf
lekiam25 <42803736+lekiam25@users.noreply.github.com>
2020-11-25 16:01:46 +0100

    Improved Door Mechanic (added transition) + new test scene
    - added transition to door script
    - made new test scene

---
+++ Project Innovation/Assets/Scripts/GameMaster/GameMaster.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public int targetFramerate = 60;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = targetFramerate;
+    }
+}
--- Project Innovation/Assets/Scripts/Interactables/Door.cs
+++ Project Innovation/Assets/Scripts/Interactables/Door.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
@@ -7,16 +8,26 @@ public class Door : MonoBehaviour
     //=======================================================================================
 
     // public objects
-    //[Header("Components")]
+    [Header("Components")]
     public Canvas victoryCanvasPrefab;
+    public GameObject doorMiddle;
 
     // public variables
     [Header("Variables")]
     public int keysNeededToOpen = 1;
+    [Min(0)]
+    public float soundLength = 4.5f; // TODO: get this from the audio source itself (google audioClip.length)
 
     // private objects
+    private Vector3 _targetPos;
+    private Vector3 _targetVector;
+    private Vector3 _targetDirection;
 
     // private variables
+    private int _soundLengthInFrames;
+
+    private float _distanceToTarget;
+    private float _travelDistance;
 
     //=======================================================================================
     //                              >  Start And Update  <
@@ -24,7 +35,8 @@ public class Door : MonoBehaviour
 
     private void Start()
     {
-
+        _targetPos = transform.Find("Back").position;   // sets the target position
+        _soundLengthInFrames = (int)(soundLength * Application.targetFrameRate);    // calculates the sound length in frames
     }
 
     private void Update()
@@ -63,7 +75,7 @@ public class Door : MonoBehaviour
             if (Player.keys >= keysNeededToOpen) // enough keys
             {
                 Player.keys -= keysNeededToOpen;
-                ChangeRoom();
+                ChangeRoom(collision);
             }
             else if (Player.keys < keysNeededToOpen && Player.keys > 0) // not enough keys
             {
@@ -82,9 +94,42 @@ public class Door : MonoBehaviour
 
     //-----------------------------------ChangeRoom-----------------------------------------
     //changes the game to the new room
-    private void ChangeRoom()
+    private void ChangeRoom(Collision collision)
     {
-        Instantiate(victoryCanvasPrefab); // WIP
-        Player.canMove = false;
+        //Instantiate(victoryCanvasPrefab); // WIP, uncomment for simple testing
+
+        Player.canMove = false; // stops the player from using controls to move
+
+        //Transition the player
+        StartCoroutine(Transition(collision));
+    }
+
+    //-----------------------------------Transition-----------------------------------------
+    //Smoothly transitions the player through a door
+    private IEnumerator Transition(Collision collision)
+    {
+        Destroy(GetComponent<BoxCollider>());   // destroys the collider so that you can move through the door
+        CalculateDistances(collision);  // calculates travel distance and direction
+
+        // moves the player towards the target
+        for (int i = 0; i < _soundLengthInFrames; i++)
+        {
+            collision.transform.position += _targetDirection * _travelDistance; // direction unit vector * speed (travel distance)
+            yield return null;
+        }
+
+        Destroy(GetComponentInChildren<AudioSource>()); // destroys the looping audio clip WIP (might mess up stuff later)
+        doorMiddle.GetComponent<BoxCollider>().enabled = true; // turns on the box Collider for the middle of the door (which is tagged as Wall)
+        Player.canMove = true;  // the player can move again after the transition
+    }
+
+    //-----------------------------------CalculateDistances-----------------------------------------
+    //Calculates the distances needed to travel and the direction
+    private void CalculateDistances(Collision collision)
+    {
+        _targetVector = _targetPos - collision.transform.position;  // the vector from the player to the target
+        _distanceToTarget = _targetVector.magnitude;                // the total distance from the player to the target (at the beginning of the transition)
+        _targetDirection = _targetVector.normalized;                // the direction from the player to the target (normalized)
+        _travelDistance = _distanceToTarget / _soundLengthInFrames; // the distance the player needs to travel each frame (to line up with the soundclip)
     }
 }
