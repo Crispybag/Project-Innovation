aa57278befcb71a708e791aaceea005b11fb16cb
lekiam25 <42803736+lekiam25@users.noreply.github.com>
2020-11-26 18:58:11 +0100

    Started with Checkpoint Script & Object

---
+++ Project Innovation/Assets/Checkpoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checkpoint : MonoBehaviour
+{
+    public static GameObject checkpoint;
+
+    private void Awake()
+    {
+        if (checkpoint == null)
+        {
+            Debug.Log("Initialize first checkpoint");
+            checkpoint = this.gameObject;
+            DontDestroyOnLoad(checkpoint);
+        }
+        else
+        {
+            Debug.Log("Destroy(this)");
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Maikel");
+        }
+    }
+}
