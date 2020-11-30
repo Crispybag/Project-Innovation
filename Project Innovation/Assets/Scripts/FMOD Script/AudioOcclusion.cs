5f3a5424326e18c3c003210da6f51bff27065480
Leo Jansen <ljansenota@gmail.com>
2020-11-30 22:34:11 +0100

    Implemented Audio Occlusion
    wow, its so simple

---
+++ Project Innovation/Assets/Scripts/FMOD Script/AudioOcclusion.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class AudioOcclusion : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string selectAudio;
+    FMOD.Studio.EventInstance audio;
+    public string parameterVolume;
+    public string parameterFrequency;
+    Transform source;
+
+    [Range(0f,1f)]
+    public float volume = 0.5f;
+    [Range(0f, 22000f)]
+    public float LPFCutoff = 10000f;
+    public LayerMask occlusionLayer = 1;
+
+    private void Awake()
+    {
+        source = GameObject.FindObjectOfType<StudioListener>().transform;
+        audio = RuntimeManager.CreateInstance(selectAudio);
+        audio.getParameterByName(parameterVolume, out volume);
+        audio.getParameterByName(parameterFrequency, out LPFCutoff);
+    }
+
+    private void Start()
+    {
+        FMOD.Studio.PLAYBACK_STATE pbState;
+        audio.getPlaybackState(out pbState);
+        if (pbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            audio.start();
+        }
+    }
+
+    private void Update()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(audio, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        RaycastHit hit;
+        Physics.Linecast(transform.position, source.position, out hit, occlusionLayer);
+
+        if (hit.collider.name == "Player")
+        {
+            notOccluded();
+            Debug.DrawLine(transform.position, source.position, Color.blue);
+        }
+        else
+        {
+            occluded();
+            Debug.DrawLine(transform.position, source.position, Color.red);
+
+        }
+    }
+
+    public string parameterName;
+    public void playSound(int pIndex)
+    {
+        //Do this to attach the sound to a gameobject for 3D effect
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(audio, transform, GetComponent<Rigidbody>());
+
+        //Do this to set a certain parameter
+        audio.setParameterByName(parameterName, pIndex);
+
+        //Start thing
+        audio.start();
+
+        //Make sure it doesnt loop
+        audio.release();
+    }
+
+    private void notOccluded()
+    {
+        audio.setVolume(1f);
+    }
+    private void occluded() 
+    {
+        audio.setVolume(volume);
+    }
+}
