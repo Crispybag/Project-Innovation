b8874aa5dc969cbeb1076e86b4ccc6122ac3f3a4
lekiam25 <42803736+lekiam25@users.noreply.github.com>
2020-12-01 16:28:47 +0100

    added keyEnemy

---
+++ Project Innovation/Assets/Scripts/Enemy Script/KeyEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyEnemy : MonoBehaviour
+{
+    public int hp;
+    [HideInInspector]public bool inCombat = false;
+
+    private MonsterCombatSounds sounds;
+    private GameObject[] enemies;
+    [HideInInspector] public bool isDying;
+
+    //public static bool isPaused = false;
+    private void Start()
+    {
+        sounds = GetComponent<MonsterCombatSounds>();
+    }
+
+    private void Update()
+    {
+        if (hp <= 0 && !isDying)
+        {
+
+            DetermineEnemyMovePhaseFaweedEditon faweed = GetComponent<DetermineEnemyMovePhaseFaweedEditon>();
+            foreach (var obj in faweed._enemies)
+            {
+                if (GetComponent<DetermineEnemyMovePhaseFaweedEditon>())
+                {
+                    DetermineEnemyMovePhaseFaweedEditon phase = obj.GetComponent<DetermineEnemyMovePhaseFaweedEditon>();
+                    phase.moveTrail.enabled = true;
+                }
+            }
+            
+            Player.keys++;
+            isDying = true;
+            sounds.playSound(2);
+        }
+    }
+}
